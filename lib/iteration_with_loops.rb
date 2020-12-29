e212988b1bdf4b3225ac61f8d73ed36cab30f720
C. L <amautacelt@gmail.com>
2020-12-29 03:58:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -65,5 +65,13 @@ def join_nested_strings(src)
   src.each do |items|
     items.each do |item|
       if item.a?(String)
+        words << item
+      end
+    end
+  end
+  
+  words.join(" ")
+  
+end
 
   
